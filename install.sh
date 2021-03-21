8149dee957996bef6cc8780668df8cfd61fc3ef3
pablo-snz <pablo.snz.brm@gmail.com>
2021-03-21 22:12:58 +0100

    PlugInstall

--- install.sh
+++ install.sh
@@ -65,6 +65,7 @@ instalationMain
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p $HOME/.config/nvim/
 cp -r regolith/nvim/* $HOME/.config/nvim/
+nvim +'PlugInstall --sync' +qa
 
 # Alacritty setting
 mkdir -p $HOME/.config/alacritty
